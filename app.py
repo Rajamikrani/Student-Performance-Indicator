d8f9e154ecc6c3d9e33d699951ac7cc126edb09d
Rajamikrani <rajamikrani12@gmail.com>
2026-06-13 17:08:56 +0545

    adding exception

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
+import sys
 
 from sklearn.preprocessing import StandardScaler
 from src.pipelines.predict_pipeline import CustomData , PredictPipeline
+from src.exception import CustomException
 
 application=Flask(__name__)
 
@@ -17,27 +19,33 @@ def index():
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
-    else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-        )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
+    try:
+        if request.method=='GET':
+            return render_template('home.html')
+        else:
+            data=CustomData(
+                gender=request.form.get('gender'),
+                race_ethnicity=request.form.get('ethnicity'),
+                parental_level_of_education=request.form.get('parental_level_of_education'),
+                lunch=request.form.get('lunch'),
+                test_preparation_course=request.form.get('test_preparation_course'),
+                reading_score=float(request.form.get('writing_score')),
+                
+                writing_score=float(request.form.get('reading_score'))
+            )
+            pred_df=data.get_data_as_data_frame()
+            print(pred_df)
+            print("Before Prediction")
 
-        predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print("after Prediction" , results)
-        return render_template('home.html',results=results[0])
+            predict_pipeline=PredictPipeline()
+            print("Mid Prediction")
+            results=predict_pipeline.predict(pred_df)
+            print("after Prediction" , results)
+            return render_template('home.html',results=results[0])
+    except Exception as e:
+        print("ERROR OCCURRED:")
+        print(str(e))
+        raise CustomException(e ,sys)
     
 if __name__=="__main__":
     app.run(host="0.0.0.0")        
